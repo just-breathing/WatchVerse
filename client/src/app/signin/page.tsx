55d0d5ac9a829d33b0f57c98067546e943d17f64
just-breathing <sundeep.reddy.n.2000@gmail.com>
2024-06-13 23:09:28 -0400

    init commit

---
+++ client/src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+const Signin: React.FC = () => {
+    return (    
+        <div>
+            sign in page
+        </div>
+      );
+}
+ 
+export default Signin;
---
+++ client/src/app/upload/page.tsx
@@ -0,0 +1,9 @@
+const UploadPage: React.FC = () => {
+    return (    
+        <div>
+            Upload page
+        </div>
+      );
+}
+ 
+export default UploadPage;
